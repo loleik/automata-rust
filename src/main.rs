9dc65dfa39feef9603b39600e7b049bdc4b46771
loleik <lolei+github@lolei.uk>
2025-08-26 18:23:40 +0100

    Tests

--- src/main.rs
+++ src/main.rs
@@ -27,35 +27,160 @@ impl DFA {
     }
 }
 
-fn simulate() {
-    let end: u8 = rand::random_range(0..10); // Maximum length for input stream
-    let dfa: DFA = DFA::new_basic_dfa();
-
-    let mut i: u8 = 0;
-    
+fn simulate(
+    dfa: DFA,
+    mode: &str,
+    test: Option<&str>
+) -> bool {
     let mut state: String = dfa.start;
 
-    for _ in 0..end {
-        let next: char = match rand::random_bool(1.0 / 2.0) {
-            true => '1',
-            false => '0'
-        }; // Generate the next value of the input stream
+    match mode {
+        "random" => {
+            let end: u8 = rand::random_range(0..10); // Maximum length for input stream    
 
-        println!("{:?} -> {:?}", &(state.clone(),next), dfa.transitions.get(&(state.clone(), next)));
+            for _ in 0..end {
+                let next: char = match rand::random_bool(1.0 / 2.0) {
+                    true => '1',
+                    false => '0'
+                }; // Generate the next value of the input stream
+                
+                state = dfa.transitions.get(&(state, next)).unwrap().clone();
+            }
+        }
+        "test" => {
+            let end: usize = test.unwrap_or("").len();
 
-        state = dfa.transitions.get(&(state, next)).unwrap().clone();
-
-        i += 1;
+            for i in 0..end {
+                let next: char = test.unwrap_or("").chars().nth(i).unwrap();
+                
+                state = dfa.transitions.get(&(state, next)).unwrap().clone();
+            }
+        }
+        _ => {}
     }
 
-    println!();
     if dfa.accept.contains(&state) {
-        println!("Yes, input stream has an even number of 0's");
+        println!("Yes, input stream has an even number of 0's (or none)");
+        true
     } else {
         println!("No, input stream as an odd number of 0's");
+        false
     }
 }
 
 fn main() {
-    simulate()
+    let dfa: DFA = DFA::new_basic_dfa();
+
+    //simulate(dfa, "random", None);
+
+    simulate(dfa, "test", Some("111"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_input() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", None);
+
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn even_input() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("00"));
+
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn odd_input() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("01"));
+
+        assert_eq!(result, false)
+    }
+
+    #[test]
+    fn all_ones() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("111"));
+
+        assert_eq!(result, true)
+    }
+
+    #[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_input() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", None);
+
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn even_input() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("00"));
+
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn odd_input() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("01"));
+
+        assert_eq!(result, false)
+    }
+
+    #[test]
+    fn single_zero() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("0"));
+
+        assert_eq!(result, false)
+    }
+
+    #[test]
+    fn even_zeros_with_ones() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("0101010"));
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn odd_zeros_with_ones() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("101010"));
+
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn long_input_even_zeros() {
+        let dfa: DFA = DFA::new_basic_dfa();
+
+        let result: bool = simulate(dfa, "test", Some("0".repeat(100).as_str()));
+        
+        assert_eq!(result, true);
+    }
+}
 }
