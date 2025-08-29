b38c1ed2671dc4ea85fbc95905b7f1d8cbcfc589
loleik <lolei+github@lolei.uk>
2025-08-29 11:38:50 +0100

    added options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod dfa;
 
 use clap::{arg, Command};
-use std::io;
+use std::io::{self, stdin};
 use dfa::{DFA, simulate};
 
 use crate::dfa::EXAMPLES;
@@ -29,16 +29,18 @@ fn main() {
 
             if mode.as_str() == "json" {}
             else if mode == "example" {
+                print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
                 for (x, _, z) in EXAMPLES {
                     println!("{x} : {z}")
                 }
 
-                let mut input = String::new();
+                let mut input: String = String::new();
                 io::stdin()
                     .read_line(&mut input)
                     .expect("Failed");
 
-                let number: usize = match input.trim().parse::<usize>() {
+                let dfa_selector: usize = match input.trim().parse::<usize>() {
                     Ok(n) => n,
                     Err(_) => {
                         println!("Invalid input, please input a number");
@@ -46,17 +48,47 @@ fn main() {
                     }
                 };
 
-                let (_, constructor, description) = EXAMPLES
+                let (_, constructor, _) = EXAMPLES
                         .iter()
-                        .find(|(id, _, _)| *id == number)
+                        .find(|(id, _, _)| *id == dfa_selector)
                         .expect("Invalid choice");
 
                 let dfa: DFA = constructor();
 
                 DFA::visualize(&dfa);
-                simulate(dfa, "random", None);
-                println!("----------");
-                DFA::de_json();   
+
+                println!("DFA constructed. Please select input type:");
+                println!("1 : Generate a random input stream\n2 : Specify input");
+
+                input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Failed");
+
+                let input_type: usize = match input.trim().parse::<usize>() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("Invalid input, please input a number");
+                        return;
+                    }
+                };
+
+                match input_type {
+                    1 => {
+                        simulate(dfa, "random", None);
+                    },
+                    2 => {
+                        io::stdin()
+                            .read_line(&mut input)
+                            .expect("Failed");
+
+                        simulate(dfa, "test", Some(&input));
+                    },
+                    _ => {println!("Invalid input")}
+                }
+
+                //println!("----------");
+                //DFA::de_json();   
             }
             else { println!("Please enter [json] or [example]") }
         },
